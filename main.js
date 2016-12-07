0ae958742d25f056de4eb624f2ed543675e488e8
Ming <kuil5230@gmail.com>
2016-12-07 15:15:00 +0800

    update

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
-var request = require('request');
-var cheerio = require('cheerio');
+const request = require('request');
+const cheerio = require('cheerio');
 
-var url = "http://www.cwb.gov.tw/V7/forecast/taiwan/Hsinchu_City.htm";
-request(url, function (err, res, body) {
-    var $ = cheerio.load(body);
-    var weather = [];
-    $('table.FcstBoxTable01 > tbody > tr').each(function () {
-        weather.push($(this).text().split('\n'));
+function getweather(callback) {
+    const url = "http://www.cwb.gov.tw/V7/forecast/taiwan/Hsinchu_City.htm";
+
+    request(url, function (err, res, body) {
+        let $ = cheerio.load(body);
+
+        let weather = [];
+        $('table.FcstBoxTable01 > tbody > tr').each(function () {
+            weather.push($(this).text().split('\n'));
+        });
+
+        let result = weather.map(function (elem) {
+            return {
+                time: elem[1].trim().split(' ')[0],
+                temp: elem[2].trim(),
+                cozy: elem[5].trim(),
+                rain: elem[6].trim()
+            }
+        });
+
+        let message = result.map(function (e) {
+            return e.time + '：  溫度：' + e.temp + '，降雨機率：' + e.rain + '，舒適度：' + e.cozy;
+        }).join('\n');
+
+        callback(err, message);
     });
-    console.log(weather);
+}
+
+getweather(function (err, message) {
+    console.log('新竹市\n' + message);
 });
