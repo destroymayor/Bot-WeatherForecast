9f6af5bdc33626c2097147a57be38b4fb4cf0344
Ming <kuil5230@gmail.com>
2016-12-07 16:16:57 +0800

    update

---
+++ getweather.js
@@ -0,0 +1,32 @@
+const request = require('request');
+const cheerio = require('cheerio');
+
+function getweather(callback) {
+    const url = "http://www.cwb.gov.tw/V7/forecast/taiwan/Hsinchu_City.htm";
+
+    request(url, function (err, res, body) {
+        let $ = cheerio.load(body);
+
+        let weather = [];
+        $('table.FcstBoxTable01 > tbody > tr').each(function () {
+            weather.push($(this).text().split('\n'));
+        });
+
+        let result = weather.map(function (elem) {
+            return {
+                time: elem[1].trim().split(' ')[0],
+                temp: elem[2].trim(),
+                cozy: elem[5].trim(),
+                rain: elem[6].trim()
+            }
+        });
+
+        let message = result.map(function (e) {
+            return e.time + '：溫度：' + e.temp + ' 降雨機率：' + e.rain;
+        }).join('\n');
+
+        callback(err, "新竹市\n" + message);
+    });
+}
+
+module.exports = getweather;
--- main.js
+++ main.js
@@ -1,34 +1,19 @@
-const request = require('request');
-const cheerio = require('cheerio');
+const login = require('facebook-chat-api');
+const getweather = require('./getweather');
 
-function getweather(callback) {
-    const url = "http://www.cwb.gov.tw/V7/forecast/taiwan/Hsinchu_City.htm";
+const account = {
+    email: '',
+    password: ''
+};
 
-    request(url, function (err, res, body) {
-        let $ = cheerio.load(body);
+login(account, function (err, api) {
+    //發送對象//
+    let id = 100000344642472;
+    let interval = 3 * 3600 * 1000;
 
-        let weather = [];
-        $('table.FcstBoxTable01 > tbody > tr').each(function () {
-            weather.push($(this).text().split('\n'));
-        });
-
-        let result = weather.map(function (elem) {
-            return {
-                time: elem[1].trim().split(' ')[0],
-                temp: elem[2].trim(),
-                cozy: elem[5].trim(),
-                rain: elem[6].trim()
-            }
-        });
-
-        let message = result.map(function (e) {
-            return e.time + '：  溫度：' + e.temp + '，降雨機率：' + e.rain + '，舒適度：' + e.cozy;
-        }).join('\n');
-
-        callback(err, message);
+    // setInterval(function () {
+    getweather(function (err, weather) {
+        api.sendMessage(weather, id);
     });
-}
-
-getweather(function (err, message) {
-    console.log('新竹市\n' + message);
+    // }, interval);
 });
